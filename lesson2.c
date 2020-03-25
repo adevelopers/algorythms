5f4d47e5cd759aa2d40c68b2da8db5e799f14afe
Kirill Khudyakov <hudiakov@gmail.com>
2020-03-26 00:43:30 +0300

    Урок 2. задание 2.

--- lesson2.c
+++ lesson2.c
@@ -12,9 +12,34 @@ int int2bin(int num) {
     }
 }
 
+
+// Задание 2.
+// Реализовать функцию возведения числа a в степень b:
+// a. без рекурсии;
+// b. рекурсивно;
+// c. *рекурсивно, используя свойство четности степени.
+int iPow(int base, int a) {
+    if (a == 0) {
+        return 1;
+    } if (a > 0) {
+        return base * iPow(base, a - 1);
+    } else {
+         return 1 / iPow(base, -a);
+    }
+} 
+
 int main() {
+
+    // Задание 1:
+    printf("\n\nЗадание 1.\n");
     for (int i=0; i< 128; i++) {
         printf("%u\n", int2bin(i));    
     }
-    
+
+
+    // Задание 2.
+    printf("\n\nЗадание 2.\n");
+    for (int i=0; i <= 16; i++) {
+        printf("2^%i=%i\n",i, iPow(2, i));
+    }
 }
